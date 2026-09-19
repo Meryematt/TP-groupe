0403df2648b35c478ef6ce80b3fb8e5ea8ba513e
Meryem Attari <meryem@Meryems-MacBook-Air.local>
2026-09-19 19:49:05 +0200

    Résolution problème API_URL

--- front/app.py
+++ front/app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import requests
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
 st.set_page_config(layout="wide")
 
-API_URL = "http://127.0.0.1:8001"
+load_dotenv()
+
+API_URL = os.environ.get("API_URL", "http://127.0.0.1:8000")
 
 st.title("**Dashboard KPI Ventes - NordCommerce**")
 
