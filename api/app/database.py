ea81f267ca9b10f8dd37d093fdce15337d60ab31
Meryem Attari <meryem@Meryems-MacBook-Air.local>
2026-09-19 19:15:41 +0200

    cryptage de données de BD

--- api/app/database.py
+++ api/app/database.py
@@ -1,17 +1,26 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://postgres:0101@localhost:5432/postgres"
- 
+load_dotenv()
+
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_NAME = os.environ.get("DB_NAME")
+
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
 engine = create_engine(DATABASE_URL)
- 
+
 SessionLocal = sessionmaker(bind=engine, autocommit=False)
- 
+
 def get_db():
     db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
-    
 
